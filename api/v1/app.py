e4506a86d9cd009547686011164cefc00ef13c4d
blindlit0 <blindlit0@gmail.com>
2024-05-27 18:02:51 -0700

    app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,46 +1,17 @@
 #!/usr/bin/python3
-"""
-Script sets up and runs a Flask web application.
-"""
-
-from flask import Flask, jsonify
+'''
+Creates Flask app; and registers the blueprint app_views to Flask instance app.
+'''
 from os import getenv
-from flask_cors import CORS
-
-from api.v1.views import app_views
+from flask import Flask
 from models import storage
-
+from api.v1.views import app_views
 
 app = Flask(__name__)
 
-CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
-
 app.register_blueprint(app_views)
 
-
-@app.teardown_appcontext
-def teardown(exception):
-    """
-    Teardown function to close the storage session.
-    """
-    storage.close()
-
-
-@app.errorhandler(404)
-def handle_404(exception):
-    """
-    handles 404 error
-    :return: Returns 404 JSON response
-    """
-    data = {
-        "error": "Not found"
-    }
-
-    resp = jsonify(data)
-    resp.status_code = 404
-
-    return(resp)
-
-
-if __name__ == "__main__":
-    app.run(getenv("HBNB_API_HOST"), getenv("HBNB_API_PORT"))
+if __name__ == '__main__':
+    HOST = getenv('HBNB_API_HOST', '0.0.0.0')
+    PORT = int(getenv('HBNB_API_PORT', 5000))
+    app.run(host=HOST, port=PORT, threaded=True)
